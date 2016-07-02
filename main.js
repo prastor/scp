90814838e868507b5959811da6332ccbf6e0060d
prastor <hiattj@gmail.com>
2016-07-02 08:56:20 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,94 @@
+// import modules
+var roleHarvester = require('role.harvester');
+var roleUpgrader = require('role.upgrader');
+var roleBuilder = require('role.builder');
+var roleRepairer = require('role.repairer');
+
+module.exports.loop = function () {
+    // check for memory entries of died creeps by iterating over Memory.creeps
+    for (let name in Memory.creeps) {
+        // and checking if the creep is still alive
+        if (Game.creeps[name] == undefined) {
+            // if not, delete the memory entry
+            delete Memory.creeps[name];
+        }
+    }
+
+    // for every creep name in Game.creeps
+    for (let name in Game.creeps) {
+        // get the creep object
+        var creep = Game.creeps[name];
+
+        // if creep is harvester, call harvester script
+        if (creep.memory.role == 'harvester') {
+            roleHarvester.run(creep);
+        }
+        // if creep is upgrader, call upgrader script
+        else if (creep.memory.role == 'upgrader') {
+            roleUpgrader.run(creep);
+        }
+        // if creep is builder, call builder script
+        else if (creep.memory.role == 'builder') {
+            roleBuilder.run(creep);
+        }
+        else if (creep.memory.role == 'repairer') {
+            roleRepairer.run(creep);
+        }
+    }
+
+    // setup some minimum numbers for different roles
+    var minimumNumberOfHarvesters = 1;
+    var minimumNumberOfUpgraders = 1;
+    var minimumNumberOfBuilders = 1;
+    var minimumNumberOfRepairers = 1;
+
+    // count the number of creeps alive for each role
+    // _.sum will count the number of properties in Game.creeps filtered by the
+    //  arrow function, which checks for the creep being a harvester
+    var numberOfHarvesters = _.sum(Game.creeps, (c) => c.memory.role == 'harvester');
+    var numberOfUpgraders = _.sum(Game.creeps, (c) => c.memory.role == 'upgrader');
+    var numberOfBuilders = _.sum(Game.creeps, (c) => c.memory.role == 'builder');
+    var numberOfRepairers = _.sum(Game.creeps, (c) => c.memory.role == 'repairer');
+	var creepSpawned="noone";
+    var name = undefined;
+
+    // if not enough harvesters
+    if (numberOfHarvesters < minimumNumberOfHarvesters) {
+        // try to spawn one
+        name = Game.spawns.Spawn1.createCreep([WORK,WORK,CARRY,MOVE,MOVE], undefined,
+            { role: 'harvester', working: false});
+			creepSpawned="harvester";
+    }
+    // if not enough upgraders
+    else if (numberOfUpgraders < minimumNumberOfUpgraders) {
+        // try to spawn one
+        name = Game.spawns.Spawn1.createCreep([WORK,CARRY,MOVE,MOVE], undefined,
+            { role: 'upgrader', working: false});
+			creepSpawned="upgrader";
+    }
+    // if not enough repairers
+    else if (numberOfRepairers < minimumNumberOfRepairers) {
+        // try to spawn one
+        name = Game.spawns.Spawn1.createCreep([WORK,WORK,CARRY,MOVE,MOVE], undefined,
+            { role: 'repairer', working: false});
+			creepSpawned="repairer";
+    }
+    // if not enough builders
+    else if (numberOfBuilders < minimumNumberOfBuilders) {
+        // try to spawn one
+        name = Game.spawns.Spawn1.createCreep([WORK,WORK,CARRY,MOVE,MOVE], undefined,
+            { role: 'builder', working: false});
+			creepSpawned="builder";
+    }
+   // else {
+   //     // else try to spawn a builder
+   //     name = Game.spawns.Spawn1.createCreep([WORK,WORK,CARRY,MOVE], undefined,
+  //          { role: 'builder', working: false});
+ //   }
+
+    // print name to console if spawning was a success
+    // name > 0 would not work since string > 0 returns false
+    if (!(name < 0) && name!=undefined) {
+        console.log("Spawned new " + creepSpawned + " creep: " + name);
+    }
+};
---
+++ role.harvester.js
@@ -0,0 +1,55 @@
+var roleBuilder = require('role.builder');
+
+module.exports = {
+    // a function to run the logic for this role
+    run: function(creep) {
+        // if creep is bringing energy to the spawn but has no energy left
+        if (creep.memory.working == true && creep.carry.energy == 0) {
+            // switch state
+            creep.memory.working = false;
+        }
+        // if creep is harvesting energy but is full
+        else if (creep.memory.working == false && creep.carry.energy == creep.carryCapacity) {
+            // switch state
+            creep.memory.working = true;
+        }
+
+        // if creep is supposed to transfer energy to the spawn
+        if (creep.memory.working == true) {
+            // try to transfer energy, if the spawn is not in range
+			 var structure = creep.pos.findClosestByPath(FIND_MY_STRUCTURES, {
+                // the second argument for findClosestByPath is an object which takes
+                // a property called filter which can be a function
+                // we use the arrow operator to define it
+                filter: (s) => (s.structureType == STRUCTURE_SPAWN
+                             || s.structureType == STRUCTURE_EXTENSION)
+                             && s.energy < s.energyCapacity
+            });
+			if (structure != undefined) {
+            if (creep.transfer(structure, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                // move towards the spawn
+                creep.moveTo(structure);
+            }
+			}
+			else {
+				roleBuilder.run(creep);
+			}
+        }
+        // if creep is supposed to harvest energy from source
+        else {
+            // find closest source
+			
+            var source = creep.pos.findClosestByPath(FIND_SOURCES);
+
+            // try to harvest energy, if the source is not in range
+            if (creep.harvest(source) == ERR_NOT_IN_RANGE) {
+                // move towards the source
+                creep.moveTo(source);
+            }
+			 
+			
+			
+			
+        }
+    }
+};
